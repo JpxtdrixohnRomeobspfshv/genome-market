082438b2203d8e8c7d606e04714cbeec3cc1172a
JpxtdrixohnRomeobspfshv <boleslavg90057427@rambler.ru>
2025-12-04 09:18:56 +0800

    add IInputVerifier__factory.ts, Impl.json, layouts-statistics-email.scss

---
+++ frontend/web/src/email/core-import-complex/main-import-serializer.ts
@@ -0,0 +1,3 @@
+import { ethers } from 'ethers';
+
+interface download-frontend-productProps { data: string }
---
+++ frontend/web/src/registry-gateway-serializer.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\registry-gateway-serializer.java
+// Generated at: 2025-10-05T10:52:18.160Z
