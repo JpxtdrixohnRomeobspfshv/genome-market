920271e95995134e1c0501fa320db8d3cbbb4f9c
JpxtdrixohnRomeobspfshv <boleslavg90057427@rambler.ru>
2025-12-04 09:19:43 +0800

    add adaptive.java, boilerplate.md, SepoliaConfig__factory.ts

---
+++ frontend/web/src/adaptive.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\adaptive.java
+// Generated at: 2025-10-05T10:52:18.159Z
---
+++ frontend/web/src/upload-download-interface.py
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\upload-download-interface.py
+// Generated at: 2025-10-05T10:52:18.154Z
